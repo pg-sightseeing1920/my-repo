ee3f759e2f8a55ce276af5d9b8e72b02e820b848
RuiTakahashi <ruitakahashi@RuiTakahashinoMacBook-Air.local>
2025-08-06 08:54:29 +0900

    Add test.yml

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -1,3 +1,5 @@
+// File: go/excellent/main_test.go
+
 package main
 
 import "testing"
@@ -6,5 +8,10 @@ func TestEvenOrOdd(t *testing.T) {
 	result := EvenOrOdd(10)
 	if result != "even" {
 		t.Errorf("Expected 'even', got '%s'", result)
+	}
+
+	result = EvenOrOdd(7)
+	if result != "odd" {
+		t.Errorf("Expected 'odd', got '%s'", result)
 	}
 }
